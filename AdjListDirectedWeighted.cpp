fd7e5b2066b86c33cf609e6ca3aed880487adad7
anshv-dev <anshv8166@gmail.com>
2025-07-03 18:50:17 +0530

    Adjacency-List

---
+++ AdjListDirectedWeighted.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    //directed graph (weight)
+    int vertex,edges;
+    cin>>vertex>>edges;
+    vector<pair<int,int> >AdjList[vertex];
+    int u,v,weight;
+    for(int i=0;i<edges;i++){
+        cin>>u>>v>>weight;
+        AdjList[u].push_back(make_pair(v,weight));
+    }
+    
+    for(int i=0;i<vertex;i++){
+        cout<<i<<" -> ";
+        for(int j=0;j<AdjList[i].size();j++){
+            cout<<AdjList[i][j].first<<" "<<AdjList[i][j].second<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
